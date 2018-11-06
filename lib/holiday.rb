10ffa68afe909efdc7ce4864ade5da12e59c8ba0
Katie Ashlin <akka726@outlook.com>
2018-11-06 02:01:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_holidays_with_bbq(holiday_hash)
    holidays.map do |holiday, supplies|
      holiday if supplies.include?("BBQ")
    end
- end/flatten.compact
+ end.flatten.compact
 end
 
 

c728e2fa78c927d0d7963f44d5e6ac1101df7266
Katie Ashlin <akka726@outlook.com>
2018-11-06 01:44:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-   holiday_hash[:Winter].map do |holiday, supplies|
+   holiday_hash[:winter].map do |holiday, supplies|
      supplies
   end.flatten
 end
